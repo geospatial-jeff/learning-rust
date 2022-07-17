c09596aeea076920dafa7b8f5c18806feeaa1a0b
geospatial-jeff <geospatialjeff@gmail.com>
2022-07-17 15:39:34 -0600

    parse IFDs and tags

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use enum_primitive::FromPrimitive;
 
 use bytes::{Bytes, Buf};
 mod tiff;
+use std::mem::size_of;
 
 
 #[tokio::main]
@@ -21,13 +22,50 @@ async fn main() {
         panic!("not a tiff")
     }
 
-    let ifd_offset = res.slice(4..8).get_u16();
+    let ifd_offset = res.slice(4..8).get_u32_le();
 
     let header = tiff::Header {
-        byte_order: tiff::ByteOrder::from_u16(order.get_u16()).unwrap(),
+        byte_order: tiff::ByteOrder::from_u16(order.get_u16_le()).unwrap(),
         tiff_version: tiff_version,
         ifd_offset: ifd_offset
     };
-
     println!("{:?}", header);
+    
+    let mut next_ifd_offset = ifd_offset as usize;
+
+    while next_ifd_offset != 0 {
+        // First 2 bytes contain number of tags in the IFD.
+        let tag_count = res.slice(next_ifd_offset..next_ifd_offset+2).get_u16_le();
+        
+        // Read each tag
+        for idx in 0..tag_count {
+            // Tags are always 12 bytes each.
+            let tag_start = next_ifd_offset + 2 + (12 * idx) as usize;
+            
+            // First 2 bytes contain tag code.
+            let tag_code = res.slice(tag_start..tag_start+2).get_u16_le();
+            
+            // Bytes 2-4 contain the tag's field type.
+            let tag_field_type = res.slice(tag_start+2..tag_start+4).get_u16_le();
+            let tag_type = tiff::TagType::from_u16(tag_field_type).unwrap();
+            let tag_size = tiff::tag_size(&tag_type);
+
+            // Bytes 4-8 contain the number of values in the tag.
+            let tag_count = res.slice(tag_start+4..tag_start+8).get_u32_le();
+
+            // Bytes 8-12 contain the tag value if it fits, otherwise an offset
+            // to where the tag value is stored.
+            let tag_value_size = tag_count * tag_size;
+
+            if tag_value_size <= 4 {
+                let tag_value = res.slice(tag_start+8..tag_start+12);
+            } else {
+                // println!("Skipping long tag {:?}", tag_code)
+            }
+        }
+        // Last 4 bytes of IFD contains offset to the next IFD
+        let start = next_ifd_offset + 2 + (tag_count * 12) as usize;
+        next_ifd_offset = res.slice(start..start+4).get_u32_le() as usize;
+    }
+
 }
--- src/tiff.rs
+++ src/tiff.rs
@@ -10,6 +10,7 @@
 // pub type SRATIONAL = (i32, i32);
 // pub type FLOAT     = f32;
 // pub type DOUBLE    = f64;
+use std::collections::HashMap;
 
 
 enum_from_primitive! {
@@ -22,9 +23,48 @@ enum_from_primitive! {
 }
 
 
+// https://github.com/georust/geotiff/blob/7dd9068b7aa9e21b7c50f737a765d9d6d089a79a/src/lowlevel.rs#L26-L62
+enum_from_primitive! {
+    #[repr(u16)]
+    #[derive(Debug,PartialEq)]
+    pub enum TagType {
+        ByteTag           = 1,
+        ASCIITag          = 2,
+        ShortTag          = 3,
+        LongTag           = 4,
+        RationalTag       = 5,
+        SignedByteTag     = 6,
+        UndefinedTag      = 7,
+        SignedShortTag    = 8,
+        SignedLongTag     = 9,
+        SignedRationalTag = 10,
+        FloatTag          = 11,
+        DoubleTag         = 12,
+    }
+}
+
+pub fn tag_size(t: &TagType) -> u32 {
+    match *t {
+        TagType::ByteTag           => 1,
+        TagType::ASCIITag          => 1,
+        TagType::ShortTag          => 2,
+        TagType::LongTag           => 4,
+        TagType::RationalTag       => 8,
+        TagType::SignedByteTag     => 1,
+        TagType::UndefinedTag      => 1,
+        TagType::SignedShortTag    => 2,
+        TagType::SignedLongTag     => 2,
+        TagType::SignedRationalTag => 8,
+        TagType::FloatTag          => 4,
+        TagType::DoubleTag         => 8,
+        _                          => 0,
+    }
+}
+
+
 #[derive(Debug)]
 pub struct Header {
     pub byte_order: ByteOrder,
     pub tiff_version: u8,
-    pub ifd_offset: u16,
+    pub ifd_offset: u32,
 }
